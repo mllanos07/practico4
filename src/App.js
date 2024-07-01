b4c5f7edfa866424653cb81addb5112a91013d00
llanos <d47001835@alumnos.itr.edu.ar>
2024-07-01 14:07:50 -0300

    calculadora de imc

--- src/App.js
+++ src/App.js
@@ -2,18 +2,54 @@ import "./App.css";
 import { useState } from "react";
 
 function App() {
-  const [inputText, setInputText] = useState("");
+  const [inputPeso, setInputPeso] = useState("");
+  const [inputAltura, setInputAltura] = useState("");
+  const [indice, setIndice] = useState("");
+  const [resultado, setResultado] = useState("");
 
-  const handleInputChange = event => {
-    setInputText(event.target.value);
+
+  const handleInputChangePeso = event => {
+    setInputPeso(event.target.value);
   };
+  const handleInputChangeAltura = event => {
+    setInputAltura(event.target.value);
+  };
+
+  const handleImc = () => {
+    setIndice (inputPeso / (inputAltura ** 2))
+  }
+
+  const handleResultado = () => {
+    if (indice < 18.5) setResultado ("Peso inferior al normal")
+    if (indice > 18.5 && indice < 24.9) setResultado ("Peso normal")
+    if (indice > 25.0 && indice < 29.0) setResultado ("Peso superior al normal")
+    if (indice > 30) setResultado ("Obesidad")
+
+    /*
+    "Peso inferior al normal"	Menos de 18.5
+    "Normal"	18.5 – 24.9
+    "Peso superior al normal"	25.0 – 29.9
+    "Obesidad"	Más de 30.0
+*/
+  }
 
   return (
     <div className="App">
       <main className="App-header">
-        <label>Ingresa un texto:</label>
-        <input type="text" value={inputText} onChange={handleInputChange} />
-        <p>Texto ingresado: {inputText}</p>
+        <label>Ingrese su peso en (kg):</label>
+        <input type="number"
+        value={inputPeso} 
+        onChange={handleInputChangePeso} />
+        <p>Texto ingresado: {inputPeso}</p>
+        <label>Ingrese su altura (mts):</label>
+        <input type="number"
+        value={inputAltura} 
+        onChange={handleInputChangeAltura} />
+        <p>Texto ingresado: {inputAltura}</p>
+        <button onClick={handleImc}>Calcular IMC</button>
+        <button onClick={handleResultado}>Este es su resultado</button>
+        {indice && <p>IMC: {indice}</p>}
+        {resultado && <p>resultado: {resultado}</p>}
       </main>
     </div>
   );
