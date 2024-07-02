4a0f755a3c08ecbb2b7a9e36d7047d8ff8ff70f6
llanos <d47001835@alumnos.itr.edu.ar>
2024-07-02 12:07:06 -0300

    calculadora de IMC formulario

--- src/App.js
+++ src/App.js
@@ -2,53 +2,63 @@ import "./App.css";
 import { useState } from "react";
 
 function App() {
-  const [inputPeso, setInputPeso] = useState("");
-  const [inputAltura, setInputAltura] = useState("");
   const [indice, setIndice] = useState("");
   const [resultado, setResultado] = useState("");
+  const [formData, setFormData] = useState({
+    altura: "",
+    peso: "",
+  });
 
-
-  const handleInputChangePeso = event => {
-    setInputPeso(event.target.value);
+  const handleChange = event => {
+    const { name, value } = event.target;
+    setFormData({
+      ...formData,
+      [name]: value,
+    });
   };
-  const handleInputChangeAltura = event => {
-    setInputAltura(event.target.value);
+
+  const handleSubmit = () => {
+    const imc = formData.peso / formData.altura ** 2;
+    setIndice(imc);
+    handleResultado(imc);
+    window.alert('Formulario enviado');
   };
 
-  const handleImc = () => {
-    const imc = inputPeso / (inputAltura ** 2)
-    setIndice(imc)
-    handleResultado(imc)
-  }
-
-  const handleResultado = (imc) => {
-    if (imc < 18.5) setResultado ("Peso inferior al normal")
-    if (imc > 18.5 && imc < 24.9) setResultado ("Peso normal")
-    if (imc > 25.0 && imc < 29.0) setResultado ("Peso superior al normal")
-    if (imc > 30) setResultado ("Obesidad")
-
-    /*
-    "Peso inferior al normal"	Menos de 18.5
-    "Normal"	18.5 – 24.9
-    "Peso superior al normal"	25.0 – 29.9
-    "Obesidad"	Más de 30.0
-*/
-  }
+  const handleResultado = imc => {
+    if (imc < 18.5) setResultado("Peso inferior al normal");
+    if (imc > 18.5 && imc < 24.9) setResultado("Peso normal");
+    if (imc > 25.0 && imc < 29.0) setResultado("Peso superior al normal");
+    if (imc > 30) setResultado("Obesidad");
+  };
 
   return (
     <div className="App">
       <main className="App-header">
-        <label>Ingrese su peso en (kg):</label>
-        <input type="number"
-        value={inputPeso} 
-        onChange={handleInputChangePeso} />
-        <p>Texto ingresado: {inputPeso}</p>
-        <label>Ingrese su altura (mts):</label>
-        <input type="number"
-        value={inputAltura} 
-        onChange={handleInputChangeAltura} />
-        <p>Texto ingresado: {inputAltura}</p>
-        <button onClick={handleImc}>Calcular IMC</button>
+        <h2>Formulario de Registro</h2>
+        <form onSubmit={handleSubmit}>
+          <div>
+            <label htmlFor="peso">Peso(Kg):</label>
+            <input
+              type="number"
+              id="peso"
+              name="peso"
+              value={formData.peso}
+              onChange={handleChange}
+            />
+          </div>
+          <div>
+            <label htmlFor="altura">Altura(mts):</label>
+            <input
+              type="number"
+              id="altura"
+              name="altura"
+              value={formData.altura}
+              onChange={handleChange}
+            />
+          </div>
+        </form>
+        <br></br>
+        <button onClick={handleSubmit}>calcular IMC</button>
         <p>IMC: {indice}</p>
         <p>resultado: {resultado}</p>
       </main>
